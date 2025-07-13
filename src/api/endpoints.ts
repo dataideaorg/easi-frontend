32fbea34a1e1a497a73ae0bf7989f911de61fd22
jumashafara <jumashafara0@gmail.com>
2025-07-13 01:04:05 -0400

    fix: updated endpoints

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -4,9 +4,9 @@ const API_URL = 'https://backend.easi.ac.ug';
 // export all the endpoints in a single object
 export const ENDPOINTS = {
     API_URL,
-    CONTACT: `${API_URL}/contact`,
-    NEWSLETTER: `${API_URL}/contact/newsletter`,
-    RESOURCES: `${API_URL}/resources`,
+    CONTACT: `${API_URL}/contact/`,
+    NEWSLETTER: `${API_URL}/contact/newsletter/`,
+    RESOURCES: `${API_URL}/resources/`,
 }
 
 
